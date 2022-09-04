ecc053a533854aa6c8f305690d6b3d8640386958
arexh <644564286@qq.com>
2022-09-04 18:45:28 +0800

    member table

---
+++ src/pages/member-table/form.tsx
@@ -0,0 +1,180 @@
+import React, { useContext } from 'react';
+import {
+  Form,
+  Input,
+  Button,
+  Grid,
+  Upload,
+  Notification,
+  Popconfirm,
+  Message,
+  Space,
+} from '@arco-design/web-react';
+import { GlobalContext } from '@/context';
+import locale from './locale';
+import useLocale from '@/utils/useLocale';
+import { IconDelete, IconDownload } from '@arco-design/web-react/icon';
+import { IconUpload } from '@arco-design/web-react/icon';
+import styles from './style/index.module.less';
+import * as XLSX from 'xlsx';
+
+const { Row, Col } = Grid;
+const { useForm } = Form;
+const emailRegex = new RegExp(/^\d{8}@mail.sustech.edu.cn$/);
+
+function SearchForm(props: {
+  onSearch: (event: any) => void;
+  setTableData: (tableData: any) => void;
+  onExportTable: (tableData: any) => void;
+  onDeleteTable: () => void;
+  searchInputRef: any;
+}) {
+  const { lang } = useContext(GlobalContext);
+  const t = useLocale(locale);
+  const [form] = useForm();
+
+  const beforeUpload = (file: File, filesList: File[]) => {
+    const fileReader = new FileReader();
+    fileReader.onloadend = function (evt) {
+      /* Parse data */
+      const bstr = evt.target.result;
+      const wb = XLSX.read(bstr, { type: 'binary' });
+      /* Get first worksheet */
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      /* Convert array of arrays */
+      const data = XLSX.utils.sheet_to_json(ws, {
+        header: 1,
+        defval: '-empty-',
+        blankrows: false,
+      });
+      /* Update state */
+      // skip header
+      data.shift();
+      // map to json
+      const illegalItems = [];
+      let lineCount = 0;
+      const jsonData = data.map((i: Array<string>) => {
+        lineCount++;
+        if (i.includes('') || !emailRegex.test(i[1])) {
+          illegalItems.push([`Line: ${lineCount}`].concat(i));
+          return {
+            name: i[0],
+            email: i[1],
+            studentId: '暂无',
+            grade: '1',
+            class: '2',
+            politicalStatus: i[2],
+          };
+        }
+        console.log(i);
+        return {
+          name: i[0],
+          email: i[1],
+          studentId: i[1].substring(0, 8),
+          grade: i[1].substring(1, 3),
+          class: i[1].substring(4, 6),
+          politicalStatus: i[2],
+        };
+      });
+      if (jsonData.length > 0) {
+        Notification.success({
+          closable: false,
+          title: '导入成功',
+          content: `总共${data.length}条数据, 成功导入${jsonData.length}条学生数据`,
+        });
+      } else {
+        Notification.error({
+          closable: false,
+          title: '导入失败',
+          content: `导入0条数据`,
+        });
+      }
+      if (illegalItems.length > 0) {
+        Notification.warning({
+          closable: false,
+          title: '警告',
+          content: `有${illegalItems.length}条非法数据:\n${JSON.stringify(
+            illegalItems
+          )}`,
+        });
+      }
+      props.setTableData(jsonData);
+    };
+    fileReader.readAsArrayBuffer(file);
+    return false;
+  };
+
+  const colSpan = lang === 'zh-CN' ? 7 : 12;
+
+  return (
+    <div className={styles['search-form-wrapper']}>
+      <Space size="large">
+        <Form form={form}>
+          <Form.Item
+            label="搜索"
+            style={{ width: 350, height: 30, marginBottom: 0 }}
+            field="searchContent"
+          >
+            <Input
+              ref={props.searchInputRef}
+              allowClear
+              placeholder="搜索学号或姓名"
+              onChange={props.onSearch}
+              onClear={() => {
+                props.searchInputRef.current.dom.value = '';
+                props.onSearch('');
+              }}
+            />
+          </Form.Item>
+        </Form>
+        <Button
+          style={{
+            display: 'inline-block',
+            width: 140,
+            height: 30,
+            float: 'right',
+          }}
+          icon={<IconDownload />}
+          onClick={props.onExportTable}
+        >
+          导出学生名单
+        </Button>
+        <Upload
+          style={{ display: 'inline-block', width: 140 }}
+          accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+          limit={1}
+          beforeUpload={beforeUpload}
+        >
+          <Button
+            style={{ display: 'inline-block', width: 140, height: 30 }}
+            type="primary"
+            icon={<IconUpload />}
+          >
+            上传学生名单
+          </Button>
+        </Upload>
+        <Popconfirm
+          title="确认清空成员列表？"
+          onOk={() => {
+            Message.success({
+              content: '清除完毕',
+            });
+            props.onDeleteTable();
+          }}
+        >
+          <Button
+            style={{ display: 'inline-block', width: 115, height: 30 }}
+            type="primary"
+            status="danger"
+            icon={<IconDelete />}
+          >
+            清空名单
+          </Button>
+        </Popconfirm>
+      </Space>
+    </div>
+  );
+}
+
+export default SearchForm;
---
+++ src/pages/member-table/index.tsx
@@ -0,0 +1,215 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Table,
+  Card,
+  PaginationProps,
+  Typography,
+  Message,
+  Button,
+  Notification,
+  Popconfirm,
+} from '@arco-design/web-react';
+import useLocale from '@/utils/useLocale';
+import SearchForm from './form';
+import locale from './locale';
+import * as XLSX from 'xlsx';
+import { zhiHuiTuanJianDb } from '@/db/zhiHuiTuanJianDb';
+import { useLiveQuery } from 'dexie-react-hooks';
+import Fuse from 'fuse.js';
+import { IconDelete, IconEdit } from '@arco-design/web-react/icon';
+
+const { Title } = Typography;
+const exportTableFileName = '团支部-成员名单.xlsx';
+const emailRegex = new RegExp(/^\d{8}@mail.sustech.edu.cn$/);
+
+function SearchTable() {
+  const t = useLocale(locale);
+  const searchInputRef = React.useRef(null);
+  const columns = [
+    {
+      title: '学号',
+      dataIndex: 'studentId',
+      sorter: (a, b) => a.studentId.localeCompare(b.studentId),
+    },
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: '邮箱',
+      dataIndex: 'email',
+      sorter: (a, b) => a.email.localeCompare(b.email),
+    },
+    {
+      title: '年级',
+      dataIndex: 'grade',
+      render: (col, recorder) => {
+        if (emailRegex.test(recorder.email)) {
+          return recorder.email.substring(1, 3);
+        } else {
+          return '-';
+        }
+      },
+    },
+    {
+      title: '班级',
+      dataIndex: 'class',
+      render: (col, recorder) => {
+        if (emailRegex.test(recorder.email)) {
+          return recorder.email.substring(4, 6);
+        } else {
+          return '-';
+        }
+      },
+    },
+    {
+      title: '政治容貌',
+      dataIndex: 'politicalStatus',
+      sorter: (a, b) => a.politicalStatus.localeCompare(b.politicalStatus),
+    },
+    {
+      title: '删除记录',
+      dataIndex: 'op',
+      render: (_, record) => (
+        <Popconfirm
+          title="确认删除该记录？"
+          onOk={() => {
+            zhiHuiTuanJianDb
+              .table('members')
+              .where('studentId')
+              .equals(record.studentId)
+              .delete();
+            Notification.success({
+              closable: false,
+              title: '删除成功',
+              content: `删除记录：${record.studentId}-${record.name}-\n${record.email}-${record.politicalStatus}.`,
+            });
+          }}
+        >
+          <Button status="danger" icon={<IconDelete />}></Button>
+        </Popconfirm>
+      ),
+    },
+  ];
+
+  const [data, setData] = useState([]);
+  const [pagination, setPatination] = useState<PaginationProps>({
+    showTotal: true,
+    pageSize: 99999,
+    current: 1,
+    pageSizeChangeResetCurrent: true,
+  });
+  const [loading, setLoading] = useState(true);
+  const [formParams, setFormParams] = useState({});
+  const [displayMember, setDisplayMember] = useState([]);
+  const members = useLiveQuery(() => {
+    const result = zhiHuiTuanJianDb.table('members').toArray();
+    result.then((res) => {
+      displayFilteredContent(res);
+    });
+    return result;
+  });
+
+  useEffect(() => {
+    fetchData();
+  }, [pagination.current, pagination.pageSize, JSON.stringify(formParams)]);
+
+  function fetchData() {
+    setLoading(false);
+    setData(members);
+  }
+
+  function onChangeTable(pagination) {
+    setPatination(pagination);
+  }
+
+  function displayFilteredContent(inputMembers) {
+    if (
+      searchInputRef == null ||
+      searchInputRef.current.dom.value.length == 0
+    ) {
+      setDisplayMember(inputMembers);
+    } else {
+      const value = searchInputRef.current.dom.value;
+      const fuse = new Fuse(inputMembers, {
+        keys: ['studentId', 'name'],
+        threshold: 0.1,
+      });
+      setDisplayMember(fuse.search(value).map((i) => i.item));
+    }
+  }
+
+  function handleSearch() {
+    displayFilteredContent(members);
+  }
+
+  function setTableData(data) {
+    const batchData = data.map((value) => {
+      return {
+        studentId: value.studentId,
+        name: value.name,
+        email: value.email,
+        politicalStatus: value.politicalStatus,
+      };
+    });
+    zhiHuiTuanJianDb
+      .table('members')
+      .bulkPut(batchData)
+      .then(() => setData(members));
+  }
+
+  function onDeleteTable() {
+    zhiHuiTuanJianDb.table('members').clear();
+  }
+
+  function onExportTable() {
+    let exportData = [];
+    if (Array.isArray(members) && members.length == 0) {
+      exportData = [
+        {
+          Name: '学生姓名',
+          Email: 'example@mail.com',
+          PoliticalStatus: '政治容貌(团员、预备党员或正式党员)',
+        },
+      ];
+    } else {
+      exportData = members.map((i) => {
+        return {
+          Name: i.name,
+          Email: i.email,
+          PoliticalStatus: i.politicalStatus,
+        };
+      });
+    }
+    const worksheet = XLSX.utils.json_to_sheet(exportData);
+    const workbook = XLSX.utils.book_new();
+    worksheet['!cols'] = [{ width: 16 }, { width: 28 }, { width: 14 }];
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Member');
+    XLSX.writeFile(workbook, exportTableFileName);
+    Message.success('导出学生名单成功！');
+  }
+
+  return (
+    <Card>
+      <Title heading={6}>{t['menu.list.searchTable']}</Title>
+      <SearchForm
+        searchInputRef={searchInputRef}
+        onDeleteTable={onDeleteTable}
+        onExportTable={onExportTable}
+        setTableData={setTableData}
+        onSearch={handleSearch}
+      />
+      <Table
+        rowKey="id"
+        loading={loading}
+        onChange={onChangeTable}
+        pagination={pagination}
+        columns={columns}
+        data={displayMember}
+      />
+    </Card>
+  );
+}
+
+export default SearchTable;
